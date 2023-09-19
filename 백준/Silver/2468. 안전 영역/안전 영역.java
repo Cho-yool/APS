7f9b60a9ba4214574354bed5c06621666f3d8413
SeongHo Cho <139312827+Cho-yool@users.noreply.github.com>
2023-09-19 17:45:14 +0900

    [Silver I] Title: 안전 영역, Time: 208 ms, Memory: 15852 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -3,18 +3,16 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N, rain, count, count_max;
-	static int[] dr = {-1, 1, 0 , 0};
-	static int[] dc = {0, 0, -1 , 1};
+	static int N, count_max, rain;
+	static int[] dr = {-1, 1, 0, 0};
+	static int[] dc = {0, 0, -1, 1};
 	static int[][] arr;
 	static boolean[][] visited;
 	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException{
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -23,7 +21,8 @@ public class Main {
 		N = Integer.parseInt(br.readLine());
 		arr = new int[N][N];
 		visited = new boolean[N][N];
-		count = 0;
+		rain = 0;
+		count_max = 1;
 		
 		for(int i=0; i<N; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -32,53 +31,38 @@ public class Main {
 			}
 		}
 		
-		if(rain == 0) count_max = 1;
-		
 		for(rain=1; rain<=100; rain++) {
 			
-			for(int i=0; i<N; i++) {
-				for(int j=0; j<N; j++) {
-					if(arr[i][j] <= rain) arr[i][j] = -1;
-				}
-			}
+			int count = 0;
 			visited = new boolean[N][N];
-			count = 0;
 			
 			for(int i=0; i<N; i++) {
 				for(int j=0; j<N; j++) {
-					if(arr[i][j] != -1 && !visited[i][j])
-						bfs(i, j);
-					if(count_max < count) count_max = count; 
+					if(!visited[i][j] && arr[i][j] > rain) {
+						visited[i][j] = true;
+						dfs(i, j);
+						count++;
+					}
 				}
 			}
-		}		
+			if(count_max < count) count_max = count;
+		}
 		
 		bw.write(String.valueOf(count_max));
 		bw.close();
 		br.close();
-		
 	}//main
 	
-	public static void bfs(int row, int col) {
-		Queue< int[]> queue = new LinkedList<int[]>();
-		queue.offer(new int[] {row, col});
-		visited[row][col] = true;
+	public static void dfs(int row, int col) {
 		
-		while(!queue.isEmpty()) {
-			int[] cur = queue.poll();
-			int r = cur[0];
-			int c = cur[1];
+		for(int i=0; i<4; i++) {
+			int nr = row + dr[i];
+			int nc = col + dc[i];
 			
-			for(int i=0; i<4; i++) {
-				int nr = r + dr[i];
-				int nc = c + dc[i];
-				
-				if(nr >=0 && nr < N && nc >=0 && nc <N && !visited[nr][nc] && arr[nr][nc] != -1) {
-					visited[nr][nc] = true;
-					queue.offer(new int[] {nr, nc});
-				}	
+			if(nr >=0 && nr < N && nc >=0 && nc < N && !visited[nr][nc] && arr[nr][nc] > rain) {
+				visited[nr][nc] = true;
+				dfs(nr, nc);
 			}
 		}
-		count++;
 	}
 }
