63148becdfe0c5284e53dc455b4ae22ad3cbf5eb
SeongHo Cho <139312827+Cho-yool@users.noreply.github.com>
2023-09-21 10:15:24 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 1892 ms, Memory: 325160 KB -BaekjoonHub

---
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
@@ -0,0 +1,93 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+import java.io.IOException;
+
+public class Main {
+
+	static int N, M;
+	static int[] parent;
+	
+	public static class Node implements Comparable<Node>{
+		int nodeA, nodeB, cost;
+		
+		public Node(int a, int b, int c) {
+			nodeA = a;
+			nodeB = b;
+			cost = c;
+		}
+		
+		public int compareTo(Node other) {
+			return Integer.compare(this.cost, other.cost);
+		}
+	}
+	
+	public static int find(int x) {
+		if(x == parent[x]) return x;
+		
+		return parent[x] = find(parent[x]);
+	}
+	
+	public static void union(int a, int b) {
+		int pa = find(a);
+		int pb = find(b);
+		
+		if(pa < pb) {
+			int temp = parent[pb];
+			parent[pb] = pa;
+			parent[temp] = pa;
+		}
+		else parent[pa] = pb;
+	}
+	
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		PriorityQueue<Node> queue = new PriorityQueue<>();
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		parent = new int[N+1];
+		for(int i=1; i<=N; i++) {
+			parent[i] = i;
+		}
+
+		for(int i=0; i<M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			
+			queue.offer(new Node(a,b,c));
+		}
+		
+		long result = 0;
+		long c_max = 0;
+		while(!queue.isEmpty()) {
+			
+			Node node = queue.poll();
+			
+			int a = node.nodeA;
+			int b = node.nodeB;
+			int c = node.cost;
+			
+			if(find(a) != find(b)) {
+				union(a,b);
+				result += c;
+				if(c_max < c) c_max = c;
+			}
+		}
+		result -= c_max;
+		
+		bw.write(String.valueOf(result));
+		bw.close();
+		
+	}
+}
