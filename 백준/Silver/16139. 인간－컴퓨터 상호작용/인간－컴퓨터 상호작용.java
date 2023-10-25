43ef0f132a9fa30a6ce8f4818ca14d6922e219d8
Cho-yool <139312827+Cho-yool@users.noreply.github.com>
2023-10-25 10:22:11 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 252 ms, Memory: 16152 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static StringTokenizer st;
+
+    public static class Test{
+        String target;
+        int start;
+        int end;
+
+    }
+
+    public static void main(String[] args) throws IOException{
+        String[] str = br.readLine().split("");
+
+        int T = Integer.parseInt(br.readLine());
+        Map<String,int[]> alpList = new HashMap<>();
+        for(int t = 0 ; t < T ; t++){
+            st = new StringTokenizer(br.readLine());
+            String target = st.nextToken();
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int[] arr;
+
+            if(alpList.containsKey(target)){
+                arr = alpList.get(target);
+            }else{
+                arr = new int[str.length];
+                arr[0] = str[0].equals(target) ? 1 : 0;
+                for(int i = 1 ; i < str.length; i++){
+                    arr[i] = arr[i-1] + (str[i].equals(target)?1:0);
+                }
+                alpList.put(target,arr);
+            }
+            System.out.println(arr[end] - (start == 0 ? 0 : arr[start-1]));
+        }
+
+
+    }
+}
