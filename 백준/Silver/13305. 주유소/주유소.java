e426e045782a046650ad115460c69c8661a122d1
Cho-yool <139312827+Cho-yool@users.noreply.github.com>
2023-10-25 11:48:07 +0900

    [Silver III] Title: 주유소, Time: 384 ms, Memory: 35652 KB, Score: 58 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static StringTokenizer st;
+
+
+    public static void main(String[] args) throws IOException{
+        int N = Integer.parseInt(br.readLine());
+
+        int[] path = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] oil = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        int cur = 0;
+        int cost = 0;
+
+        for(int i = 0 ; i < N-1; i++){
+//            System.out.println(i + " : cur = " + cur + " cost = " + cost);
+            if(cur == 0){
+                cur +=path[i];
+                cost += path[i] * oil[i];
+
+                for(int x = i+1; x < N-1; x++){
+                    if(oil[i] > oil[x]){
+                        break;
+                    }
+                    cur +=path[x];
+                    cost += path[x] * oil[i];
+                }
+            }
+            cur -= path[i];
+        }
+        System.out.println(cost);
+    }
+}
