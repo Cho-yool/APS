865a595efce4d4706af2accee4a3d1352ef38a91
Cho-yool <139312827+Cho-yool@users.noreply.github.com>
2023-11-03 11:49:46 +0900

    [Silver I] Title: 나이트의 이동, Time: 456 ms, Memory: 124632 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
@@ -1,68 +1,77 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
 
 public class Main {
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static StringTokenizer st;
+    public static int INF = Integer.MAX_VALUE;
+    public static int[] dx = {1,2,2,1,-1,-2,-2,-1};
+    public static int[] dy = {-2,-1,1,2,2,1,-1,-2};
+    public static class Pos{
+        int x;
+        int y;
+        int count = 0;
 
-	static int I, answer, startRow, startCol, endRow, endCol;
-	static int[] dr = {-1, -2, -2, -1, 1, 2, 2, 1};
-	static int[] dc = {-2, -1, 1, 2, 2, 1, -1, -2};
-	static int[][] arr;
-	static boolean[][] visited;
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	static StringTokenizer st;
-	
-	public static void main(String[] args) throws IOException{
-		
-		int T = Integer.parseInt(br.readLine());
-		
-		for(int tc=1; tc<=T; tc++) {
-			
-			I = Integer.parseInt(br.readLine());
-			st = new StringTokenizer(br.readLine());
-			startRow = Integer.parseInt(st.nextToken());
-			startCol = Integer.parseInt(st.nextToken());
-			
-			st = new StringTokenizer(br.readLine());
-			endRow = Integer.parseInt(st.nextToken());
-			endCol = Integer.parseInt(st.nextToken());
-			
-			answer = 0;
-			arr = new int[I][I];
-			visited = new boolean[I][I];
-			
-			bfs(startRow, startCol);
-			answer = arr[endRow][endCol];
-			bw.write(String.valueOf(answer) + "\n");
-		}//tc
-		bw.close();
-	}//main
-	
-	public static void bfs(int row, int col) {
-		Queue<int[]> queue = new LinkedList<int[]>();
-		queue.offer(new int[] {row, col});
-		visited[row][col] = true;
+        public Pos(int x, int y, int count){
+            this.x = x;
+            this.y = y;
+            this.count= count;
+        }
+        public Pos(String x, String y){
+            this.x = Integer.parseInt(x);
+            this.y = Integer.parseInt(y);
+        }
+        public boolean equals(Pos o){
+            if(this.x == o.x && this.y == o.y)return true;
+            else return false;
+        }
+    }
 
-		while(!queue.isEmpty()) {
-			int[] cur = queue.poll();
-			int r = cur[0];
-			int c = cur[1];
-			
-			for(int i=0; i<8; i++) {
-				int nr = r + dr[i];
-				int nc = c + dc[i];
-				
-				if(nr<0 || nc<0 || nr>=I || nc>=I || visited[nr][nc]) continue;
-				arr[nr][nc] = arr[r][c] + 1;
-				queue.offer(new int[] {nr, nc});
-				visited[nr][nc] = true;
-			}
-		}
-	}
+    public static void main(String[] args) throws IOException{
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+        for(int t = 0; t < T; t++){
+            int N = Integer.parseInt(br.readLine());
+            int[][] board=  new int[N][N];
+            for(int i = 0 ; i < N; i++){
+                Arrays.fill(board[i],INF);
+            }
+
+            st = new StringTokenizer(br.readLine());
+            Pos start = new Pos(st.nextToken(),st.nextToken());
+
+            st = new StringTokenizer(br.readLine());
+            Pos target = new Pos(st.nextToken(),st.nextToken());
+
+            Queue<Pos> qu = new ArrayDeque<>();
+            qu.add(start);
+            while(!qu.isEmpty()){
+                Pos cur = qu.poll();
+
+                if(board[cur.y][cur.x] != INF) continue;
+
+                board[cur.y][cur.x] = cur.count;
+
+                if(cur.equals(target)){
+                    sb.append(board[cur.y][cur.x]).append("\n");
+                    break;
+                }
+
+                for(int i = 0; i < 8; i++){
+                    int mx = cur.x + dx[i];
+                    int my = cur.y + dy[i];
+                    if(mx < 0 || my < 0 || mx >= N || my >= N || board[my][mx] != INF) continue;
+
+                    qu.add(new Pos(mx,my,cur.count+1));
+                }
+            }
+
+        }
+
+        System.out.print(sb);
+    }
+
 }
